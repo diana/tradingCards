a02bf57a1ea6438d692f681b1777cb63eccbccdf
github username <github email address>
2019-11-12 13:05:57 -0700

    added update and destroy to owner controller

--- backend/app/controllers/owners_controller.rb
+++ backend/app/controllers/owners_controller.rb
@@ -2,19 +2,30 @@ class OwnersController < ApplicationController
 
     def index
         owners = Owner.all
-        render json: owners
+        render json: owners, include: [:cards]
     end
 
     def show
         owner = Owner.find(params[:id])
-        render json: card
+        render json: card, include: [:cards]
     end
 
     def create
         owner = Owner.create(
-            name: params[:name],
+            name: params[:name]
         )
         redirect_to 'https://localhost:3001/'
     end
 
+    def update 
+        owner = Owner.find(params[:id])
+        owner.update(name: params[:name])
+        render json: owner, include: [:cards]
+    end 
+
+    def destroy 
+        owner = Owner.find(params[:id])
+        owner.update
+        render json: {status: 204} 
+    end 
 end
